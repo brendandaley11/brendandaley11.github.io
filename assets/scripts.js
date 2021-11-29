a1cdcb7a763b804d72843f01ec7d56af4ff93a05
Brendan <brendandaley11@gmail.com>
2021-11-29 16:30:45 +1100

    Finalised portfolio
    Added all assets needed, tidied up CSS/HTML code, finished Projects, Key Skills and Contact sections. Can potentially add more projects or fancier animations to add flair to portfolio.

---
+++ assets/scripts.js
@@ -0,0 +1,25 @@
+$(window).scroll(function () {
+    if($(this).scrollTop() > window.innerHeight){
+        $('.grid').addClass('fixed');
+    }else{
+        $('.grid').removeClass('fixed');
+    }
+});
+
+function openProject(evt, projName) {
+    var i, tabcontent, tablinks
+
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++)
+    {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    document.getElementById(projName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
